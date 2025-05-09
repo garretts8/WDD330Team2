c70dab4be77f6083290d16f5426df740c6a00b49
Hyrum Whittier <whittierht@gmail.com>
2025-05-09 09:49:42 -0600

    x instead of remove to follow requirments

--- src/js/cart.js
+++ src/js/cart.js
@@ -38,7 +38,7 @@ function cartItemTemplate(item) {
     <p class="cart-card__color">${item.Colors[0].ColorName}</p>
     <p class="cart-card__quantity">qty: ${item.quantity}</p>
     <p class="cart-card__price">$${item.FinalPrice}</p>
-    <button class="remove-item" data-id="${item.Id}">Remove</button>
+    <button class="remove-item" data-id="${item.Id}">X</button>
   </li>`;
 
   return newItem;
