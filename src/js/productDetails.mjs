2a4919099a8689d252d75b47a16096b8b907d8ce
garretts8 <gar21085@byui.edu>
2025-05-10 12:19:09 -0600

    Fixed problem with image

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,35 +1,30 @@
 import { findProductById } from "./productData.mjs";
 import { setLocalStorage } from "./utils.mjs";
 
-// three functions are recommended: productDetails(productId), addToCart() -moved from the 
-// product.js, and renderProductDetails().
-
-//This function will become the entrypoint into our module and will make sure that 
-// everything happens in the right order. This function should be the default export.
 let product = {};
 
 export default async function productDetails(productId) {
+  // get the details for the current product. findProductById will return a promise! use await or .then() to process it
   product = await findProductById(productId);
+  // once we have the product details we can render out the HTML
   renderProductDetails();
-  document
-    .getElementById("addToCart").addEventListener("click", addToCart);
+  // once the HTML is rendered we can add a listener to Add to Cart button
+  document.getElementById("addToCart").addEventListener("click", addToCart);
 }
-
 function addToCart() {
   setLocalStorage("so-cart", product);
 }
 
-// method to fill in the details for the current product in the HTML.
 function renderProductDetails() {
-  document.querySelector("#productName").innerHTML = product.Brand.Name;
-  document.querySelector("#productNameWithoutBrand").innerHTML = product.NameWithoutBrand;
-  document.querySelector("#productImage").src = product.Images;
-  document.querySelector("#productImage").alt = product.Name;  
-  document.querySelector("#productFinalPrice").innerHTML = product.FinalPrice; 
-  document.querySelector("#productColorName").innerHTML = product.Colors[0].ColorName; 
-  document.querySelector("#productDescriptionHtmlSimple").innerHTML = product.DescriptionHtmlSimple;
+  document.querySelector("#productName").innerText = product.Brand.Name;
+  document.querySelector("#productNameWithoutBrand").innerText =
+    product.NameWithoutBrand;
+  document.querySelector("#productImage").src = product.Image;
+  document.querySelector("#productImage").alt = product.Name;
+  document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+  document.querySelector("#productColorName").innerText =
+    product.Colors[0].ColorName;
+  document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+    product.DescriptionHtmlSimple;
   document.querySelector("#addToCart").dataset.id = product.Id;
 }
-/* product name,  product without brand, product image source, product image alt, productFinalPrice, productColorName, productDescriptionHtmlSimple, addToCart */
-
-// Handles Add to Cart clicks
