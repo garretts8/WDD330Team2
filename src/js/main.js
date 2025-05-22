26737a02bbee12f4beabf5967f5c32076db31029
Hyrum Whittier <whittierht@gmail.com>
2025-05-22 08:36:18 -0600

    newsletter!

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,14 @@ productList(".product-list", "tents");
 loadHeaderFooter().then(() => {
   updateCartIconCount(); 
 });
+
+const form = document.getElementById("newsletter-form");
+if (form) {
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    const email = document.getElementById("newsletter-email").value;
+    // TODO: send `email` to your newsletter backend
+    alert(`Thanks for subscribing, ${email}!`);
+    form.reset();
+  });
+}
