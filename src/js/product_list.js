bcfa716674676598e95d5dfc52a3ab2f64d8a133
Hyrum Whittier <whittierht@gmail.com>
2025-07-21 18:17:01 -0600

    maybe fixes it

--- src/js/product_list.js
+++ src/js/product_list.js
@@ -2,8 +2,13 @@ import { productList } from "./productList.mjs";
 import { getParam, loadHeaderFooter, updateCartIconCount } from "./utils.mjs";
 
 const category = getParam("category");
-// productList(".product-list", "tents");
-productList(".product-list", category);
+
+if (!category) {
+  alert("Missing category in URL. Redirecting to home.");
+  window.location.href = "/"; // or show an error message
+} else {
+  productList(".product-list", category);
+}
 
 loadHeaderFooter().then(() => {
   updateCartIconCount(); 
